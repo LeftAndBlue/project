d8eacbbb00d07ba5b285f620ea214e10e3a2d381
陈志明 <jamin.chen@searching-info.com>
2023-11-15 17:47:02 +0800

    获取表结构并识别数据库的主键。

--- DatabaseManager/DbManager/service/MysqlCon.cs
+++ DatabaseManager/DbManager/service/MysqlCon.cs
@@ -1,5 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Configuration.Provider;
+using System.Data;
+using System.Data.Common;
 
 namespace DbManager.service
 {
@@ -7,20 +12,36 @@ namespace DbManager.service
     {
 
 
-        private readonly MySqlConnection _connection;
+        public string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
 
         public MysqlCon()
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
 
-            _connection = new MySqlConnection(connectionString);
-            _connection.Open();
         }
-
-        public MySqlConnection GetConnection()
+        public MysqlCon(string str)
         {
-            return _connection;
-        }
 
+        }
+        //获取表结构并识别数据库的主键。
+        public void Entitybase(string tableName)
+        {
+            DbProviderFactory factory = MySql.Data.MySqlClient.MySqlClientFactory.Instance;
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                DataTable columnsSchema = connection.GetSchema("Columns", new string[] { null, null, tableName });
+                foreach (DataRow columnRow in columnsSchema.Rows)
+                {
+                    string columnName = columnRow["COLUMN_NAME"].ToString();
+                    string columnKey = columnRow["COLUMN_KEY"].ToString();
+                    if (columnKey == "PRI")
+                    {
+                        Console.WriteLine($"Primary key found: {columnName}");
+                    }
+                }
+            }
+        }
     }
 }
